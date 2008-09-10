5964fd02e5400711834b19e039f91068c279efc4
Tomohiro Nishimura <tomohiro68@gmail.com>
2008-09-10 23:15:12 +0900

    write some tests

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,8 +1,20 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
   # Replace this with your real tests.
+  def setup 
+    @controller = WelcomeController.new 
+    @request    = ActionController::TestRequest.new 
+    @response   = ActionController::TestResponse.new
+  end
+
   def test_truth
     assert true
+  end
+
+  def test_jump_to_login_form
+    get :login
+    assert_response :success
+    assert_template 'login'
   end
 end
