eaf0960bcb186ea4cd7501940acdfda0a5e140e8
Tomohiro Nishimura <tomohiro68@gmail.com>
2008-11-26 20:52:10 +0900

    Fix: download page is more useful

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -2,17 +2,7 @@ class MenuController < ApplicationController
   before_filter :authorize
 
   def index
-    user = User.find(session[:user_id])
-    images =
-      if user.group.nil?
-        # user doesn't belong to group
-        user.images
-      else
-        # user belongs to group
-        user.group.users.map {|u| u.images }.flatten
-      end
-    images = images.sort_by {|image| image.created_at }.reverse
-    @images = images.paginate(:page => params[:page], :per_page => 15)
+    @images = pagenated_image_for(session[:user_id])
   end
 
   def my_page
@@ -52,8 +42,17 @@ class MenuController < ApplicationController
   private
 
   def pagenated_image_for(user_id)
-    user = User.find user_id
-    user.images.paginate :page => params[:page], :per_page => 15, :order => 'created_at desc'
+    user = User.find(user_id)
+    images =
+      if user.group.nil?
+        # user doesn't belong to group
+        user.images
+      else
+        # user belongs to group
+        user.group.users.map {|u| u.images }.flatten
+      end
+    images = images.sort_by {|image| image.created_at }.reverse
+    images.paginate(:page => params[:page], :per_page => 15)
   end
 end
 
